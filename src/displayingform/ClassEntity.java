b4ad10c41c316aa4f1bbfe6e7736ea70567dc83d
ranjan <ranjan@lftechology.com>
2013-03-15 15:55:51 +0545

    this is form display

---
+++ src/displayingform/ClassEntity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package displayingform;
+
+ 
+
+public class ClassEntity {
+  
+    public String name;
+    public String address;
+    
+}
---
+++ src/displayingform/Extract.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package displayingform;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+/**
+ *
+ 
+ */
+public class Extract {
+       Field[] fields;
+        String[] valextract;
+    public void printAttribute(Class cl){
+       fields = cl.getFields();
+        valextract = new String[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            valextract[i] = fields[i].getName().toString();
+    //       System.out.println(fields[i].getType().getName().toString()+ " "+fields[i].getName().toString());
+//String first = fields[i].getName().toString();
+
+        }
+        
+
+    }
+
+    public void javaToHtmlConverter() throws IOException {
+        String str; 
+        StringBuilder htmlform = new StringBuilder();
+        htmlform.append("<html><head><title>Java To HTML</title></head><body><table><tr>");
+        try {   
+               File file = new File("myform.html");
+                 FileWriter writer = new FileWriter(file);
+            try (BufferedWriter bufferwriter = new BufferedWriter(writer)) {
+                int i =0;
+                
+                while(i<valextract.length){
+                    str= valextract[i];
+                    htmlform.append("<td>");
+                    htmlform.append(str);
+                    htmlform.append("</td><td><input type=text name="+str+"name");
+                    htmlform.append("</td></tr>");
+                    i++;
+               }
+                htmlform.append("</table></body>");
+                htmlform.append("<input type=submit value=submit>");
+//                 System.out.println("output is:"+htmlform);
+               
+               bufferwriter.write(htmlform.toString());
+            }
+                
+        }
+           catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+       
+        
+
+
+    
+
+    public static void main(String[] args) throws Exception {
+        Extract ext = new Extract();
+        ext.printAttribute(Class.forName("displayingform.ClassEntity"));
+        ext.javaToHtmlConverter();
+    }
+    }
+
